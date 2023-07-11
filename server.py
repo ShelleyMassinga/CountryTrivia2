4d6112d95333bc17bd0bbf0c97efc72b3485928f
kidus100 <kiyati.tech@gmail.com>
2023-07-11 18:08:56 -0400

    recieved data from interactive map

--- server.py
+++ server.py
@@ -14,5 +14,15 @@ def sign_up():
 def login():
     return render_template("login.html")
 
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
+@app.route('/test/<name>', methods=['GET'])
+def levels(name):
+    difficulty = ['Easy', 'Medium', 'Difficult']
+    return render_template("quiz.html", difficulty = difficulty)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
